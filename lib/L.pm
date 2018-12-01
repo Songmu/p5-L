c2ceec360bd2d3bb15f127d66a11cbb696677f3e
Nick Tonkin <1nickt@users.noreply.github.com>
2018-12-01 11:47:39 -0500

    Fix some typos in POD

--- lib/L.pm
+++ lib/L.pm
@@ -37,7 +37,7 @@ __END__
 
 =head1 NAME
 
-L - Perl extention to load module automatically in one liner.
+L - Perl extention to load modules automatically in one-liners.
 
 =head1 VERSION
 
@@ -49,10 +49,10 @@ This document describes L version v1.0.0.
 
 =head1 DESCRIPTION
 
-Module auto loader for one liner.
+Module auto-loader for one-liners.
 
-This modules is dangerous, then don't use this module in other perl modules, scripts or product code.
-This should be used only in one liner.
+This module is dangerous, so don't use this module in other perl modules, scripts or production code.
+This should be used only in one-liners.
 
 =head1 DEPENDENCIES
 
